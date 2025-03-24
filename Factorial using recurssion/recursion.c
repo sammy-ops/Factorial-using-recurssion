16b46ab53cc566bd999adf06d288fd7692463e47
SAMAR MAURYA <72241828+sammy-ops@users.noreply.github.com>
2025-03-24 16:52:49 +0530

    Add files via upload

---
+++ Factorial using recurssion/recursion.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int facto(int);
+void main()
+{
+    int n;
+    int c;
+    scanf("%d", &n);
+    c=facto(n);
+    printf("%d", c);
+}
+int facto(int n)
+{
+    if(n<=1)
+    {
+        return(1);
+    }
+    else
+    {
+        return(n*facto(n-1));
+    }
+}
